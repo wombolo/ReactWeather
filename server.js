283b753479ec3818db9a04d8d3c50d70588a6f9a
Didun Morodolu <dmorodolu@gmail.com>
2018-07-19 08:33:46 +0100

    Basic Heroku Config in server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var express = require('express');
+let express = require('express');
 
 //Create
-var app = express();
+let app = express();
+const PORT  = process.env.PORT || 3000;
+
+app.use((req, res, next) =>{
+    if (req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    } else {
+        res.redirect('http://'+ req.hostname + req.url);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-    console.log('Express Server on port 3000');
+
+app.listen(PORT, function () {
+    console.log('Express Server on port '+ PORT);
 });
