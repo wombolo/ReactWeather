6e3015642c2a61cc44c7b42bb390f0d718687820
Didun Morodolu <dmorodolu@gmail.com>
2018-07-21 11:14:31 +0100

    Add grid style to Examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,16 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className={"text-center"}>Examples</h1>
+      <p>Here are few example locations to try</p>
+        <ol>
+            <li><Link to={'/?location=Abeokuta'} >Abeokuta</Link></li>
+            <li><Link to={'/?location=New york'} >New york</Link></li>
+
+        </ol>
     </div>
   )
 };
